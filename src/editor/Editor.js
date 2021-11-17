8a880efacb3e60c46ec3826a54daaad09737d8f2
Varghese Mathai <varghesemathaime@gmail.com>
2021-11-17 23:21:08 +0100

    Editor outline created with quicktools

---
+++ src/editor/Editor.js
@@ -0,0 +1,9 @@
+import QuickTools from "./quicktools/QuickTools";
+import TopMenu from "./topmenu/TopMenu";
+
+export default function Editor() {
+    return <div>
+        <QuickTools/>
+        <TopMenu/>
+    </div>;
+}
